929a5fd7f859bcd3d7c96ae3f7dbe19f4c95affe
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-02-23 02:56:01 +0530

    Time: 11 ms (69.02%), Space: 42.4 MB (84.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,21 +3,21 @@ class Solution {
         if(s.length()!=t.length())
         return false;
         else{
-            HashMap<Character,Character> mp1=new HashMap<>();
-            HashMap<Character,Boolean> mp2=new HashMap<>();
-            for(int i=0;i<s.length();i++){
+            HashMap<Character,Character> mp1=new HashMap<>();//for store mapping of s with t
+            HashMap<Character,Boolean> mp2=new HashMap<>();//for stor the flag for 
+            for(int i=0;i<s.length();i++){//both the string of equal size take any
                 char ch1=s.charAt(i);
                 char ch2=t.charAt(i);
                 if(mp1.containsKey(ch1)){
-                    if(mp1.get(ch1)!=ch2)
+                    if(mp1.get(ch1)!=ch2)//now we are check the condition where mapping fail
                     return false;
                 }
                 else{
                     if(mp2.containsKey(ch2)){
-                        return false;
+                        return false;//if mp1 not contains that key,and the ch2 is mapp with someone
 
                     }
-                    else{
+                    else{//if no false return till now mapp the ch1 with ch2 and mark ch2 flag true
                         mp1.put(ch1,ch2);
                         mp2.put(ch2,true);
                     }
