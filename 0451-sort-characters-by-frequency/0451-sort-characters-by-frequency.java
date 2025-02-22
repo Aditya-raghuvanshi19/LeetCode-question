21d6074c4ef5a63f5a9959c613c63107d9c5dfbf
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-02-23 03:02:54 +0530

    Time: 16 ms (44.03%), Space: 45.6 MB (71.41%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -6,9 +6,9 @@ class Solution {
         }
         StringBuilder ans=new StringBuilder();
         List<Character> list=new ArrayList(mp.keySet());
-        list.sort((k1,k2)->mp.get(k2)-mp.get(k1));
+        list.sort((k1,k2)->mp.get(k2)-mp.get(k1));//sort in increasing order
         for(char ch:list){
-            for(int i=0;i<mp.get(ch);i++){
+            for(int i=0;i<mp.get(ch);i++){//the number of frequency of that particular character ch ,that many times it store in the answer string.
                 ans.append(ch);
             }
         }
