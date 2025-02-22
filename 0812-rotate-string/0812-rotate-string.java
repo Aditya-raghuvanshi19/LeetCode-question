b2f150012b4eb69c975cf071465297f1cb3d52a6
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-02-23 02:58:50 +0530

    Time: 0 ms (100%), Space: 41.4 MB (64.41%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         return s.length()==goal.length() && (s+s).contains(goal);
-    }
+    }//in the double length of string s ,it is defently that the correct rotation will come under s+s ,try dry run
 }
