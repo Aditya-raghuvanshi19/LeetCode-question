9fa61c267b65b5af00862c4c6f47efacd2efbd78
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-05-19 19:44:06 +0530

    Time: 0 ms (100%), Space: 42.1 MB (77.22%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,8 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[i-m];
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        
+        while(j>=0){ //this is becuase nums2 always have element but 1st may or may not so only run loop till we cover all ement of 2nd
+            if(i>=0 && nums1[i]>nums2[j]){//here i check because if 1st not have element than i become -1 and give error so better to check that i>=0 it means 1st have elemnt.
+                nums1[k]=nums1[i];
+                i--;
+            }else
+            {
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+            
         }
-        Arrays.sort(nums1);
+
     }
 }
