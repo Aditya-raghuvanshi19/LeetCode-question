75eb09e44c7bbeb639a4d0fd8e757bf1c123f120
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-05-19 20:08:36 +0530

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -11,3 +11,14 @@ class Solution {
         return count;
     }
 }
+
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         int i=0;
+//        for(int n:nums){
+//         if(n!=val)
+//         nums[i++]=n;
+//        }
+//        return i;
+//     }
+// }
