b80b2867ae77cb4391932853dc64c1ebb189c8c4
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-05-20 13:20:45 +0530

    Time: 0 ms (100%), Space: 46.7 MB (62.94%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int removeDuplicates(int[] nums) {
        int i=0;
        for(int n: nums){
-        if(i<2 || n!=nums[i-2]){
+        if(i<2 || n!=nums[i-2]){//this i is check becuase first two value till index 1 is always correct either duplicate or single after that index , if more than two occure store location in i and traverse the array when diff. number found put at the stored location
             nums[i]=n;
             i++;
         }
