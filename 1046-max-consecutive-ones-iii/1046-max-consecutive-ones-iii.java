2e49de346debafa2051776ae8a29a8efd19a0ed5
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-05-29 18:57:52 +0530

    Update 1046-max-consecutive-ones-iii.java

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -19,3 +19,24 @@ class Solution {
         return maxone;
     }
 }
+
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//        int l=0;
+//        int r=0;
+//        int n=nums.length;
+//        int maxone=Integer.MIN_VALUE;
+//        int zero=0;
+//        while(r<n){
+//         if(nums[r]==0)zero++;
+//         if(zero>k){//due to this code become in O(N) not 2N in erlier their is two while loop which cause 2N.
+//             if(nums[l]==0)zero--;
+//             l++;
+//         }
+//         if(zero<=k)
+//         maxone=Math.max(maxone,r-l+1);
+//         r++;
+//        }
+//        return maxone;
+//     }
+// }
