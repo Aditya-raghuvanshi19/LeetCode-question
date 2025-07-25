5b4f9194a48da96c2422cf401e46b160846a02c6
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-07-26 01:31:06 +0530

    Time: 6 ms (48.43%), Space: 46.2 MB (21.51%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //in the going from columm than row in the L shape directiion we found that it is incresing and anywhere else it is not continuously sorted 
+        //ex: 1,4,7,11,15,19,22,24,30 
+
+        //so putting low at the starting of row and high at the end of row elem ex: 15 
+        //if high is greater than target it means that column below to high is useless ex: 19,22,24,30
+        //so its better to leave them and move high--; same goes for low 
+        //low goes top to bottom high goes right to left
+
+        int low=0;
+        int high=matrix[0].length-1;
+
+        while(low < matrix.length && high>=0){
+            if(matrix[low][high] == target)return true;//if ask index return array of low,high 
+            //new int[]{low,high};
+            else if(matrix[low][high] > target) high--;
+            else
+            low++;
+        }
+
+        return false;//new int[]{-1,-1};
+    }
+}
