ec397055bd84480b7b8c71816bbc34267154f48d
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-08 18:53:03 +0530

    Time: 236 ms (5.04%), Space: 49.1 MB (83.73%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,30 +1,23 @@
 class Solution {
-    int simpleSieve(int n){
-        if(n==0||n==1)
-        return 0;
+    public int simplesieve(int n){
+        int count=0;
         boolean bool[] = new boolean[n];
         Arrays.fill(bool,true);
-        bool[0]=bool[1]=false;
-        int count=0;
-
-        for(int i=2; i<=Math.sqrt(n); i++){
-            for(int j = i*i; j<n; j+=i){
-                if(bool[j]){
-                    bool[j] = false;
-                }
+        bool[0]=false;
+        bool[1]=false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            for(int j=i*i; j<n; j+=i){
+                if(bool[j])
+                bool[j]=false;
             }
         }
-        for(boolean f: bool){
-            if(f)
-            count++;
+        for(boolean b: bool){
+            if(b)count++;
         }
         return count;
     }
-   
     public int countPrimes(int n) {
-    
-    if(n==0||n==1)
-    return 0;
-        return simpleSieve(n);
+        if(n==0 || n==1)return 0;
+        return (simplesieve(n));
     }
 }
