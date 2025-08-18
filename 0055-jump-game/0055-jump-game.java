a93add842180816793c607a8ee49a27a5969bec8
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-18 22:39:16 +0530

    Time: 3 ms (47.6%), Space: 46.4 MB (8.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-      int maxj=0;
-      
-      for(int i=0;i<nums.length;i++){
-          if(i>maxj)
-          return false;
-         maxj=Math.max(maxj,i+nums[i]);
-      }
-      return true;
+        //here we traverse the array for check and by traversing we can go to end ,but acc to question we can only reached at a index if it comes by adding value with the index i.e. the max index we can reached , if we traverse and reched at a index which is not possible acc to question the answer will be false becuase we can only at max reached at that index which is in maxjump
+        int maxjump=0;
+        for(int i=0;i<nums.length;i++){
+           if(i>maxjump)return false;
+           int jump=i+nums[i];
+           maxjump=Math.max(maxjump,jump);
+        }
+        return true;
     }
 }
