e47d73d40aca7bea0bbc757986346e4014225403
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-18 14:46:29 +0530

    Time: 1 ms (90.01%), Space: 45.3 MB (21.66%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,19 +1,30 @@
 class Solution {
     public int maxProduct(int[] nums) {
-       int pre=1;
-       int suff=1;
-       int n=nums.length;
-       int max=Integer.MIN_VALUE;
-       for(int i=0;i<nums.length;i++){
-        if(pre==0)//here we check it first because in the new iteration we do not want to take the zero so before further multiply we first set it as one, but if 
-         pre=1;
-        if(suff==0)
-        suff=1;
-        pre *=nums[i];//after multiply we check the condition than we set the value 1 and the corrent going iteration fails, it may be that in the currebntn iteration the 0 will be the max and the max sum we go 0 for better dry run the test case Input: nums = [-2,0,-1] Output: 0
-        suff *= nums[n-i-1];
-        
-        max=Math.max(max,Math.max(pre,suff));
-       }
-       return max;
+        // int prod=1;
+        // int maxpro=Integer.MIN_VALUE;
+        // for(int i:nums){
+        //     prod *= i;
+        //     maxpro=Math.max(maxpro,prod);
+        //     if(prod==0)
+        //     prod=1;
+           
+        // }
+        // return maxpro;
+        //the above sol works only if thier is even number of negative because in that case the product of entire array will give you the max prod if zero not occurs if zero occur we handle it by zero
+        //but if the negative number comes odd number of times so in that case the prod before the last negative number can give the max prod(prefix prod) orr product after the first negatuve will give the max pro(suffix prod) because on including the odd no's negative will lead negative or can say less prod 
+        //better to proceed from the both side
+
+        int pre=1;
+        int max=Integer.MIN_VALUE;
+        int suff=1;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            pre *= nums[i];
+            suff *= nums[n-i-1];
+            max=Math.max(max,Math.max(pre,suff));
+            if(pre==0)pre=1;
+            if(suff==0)suff=1;
+        }
+        return max;
     }
 }
