0422643511d344c9c3dfa49f2e611831d9bdec4f
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-09-11 17:28:06 +0530

    Time: 1 ms (99.86%), Space: 45.4 MB (62%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,26 @@
 class Solution {
+    public int f(int i,int j,int n,List<List<Integer>> triangle,Integer[][] dp){
+        if(i==n-1)return triangle.get(n-1).get(j);
+
+        if(dp[i][j] != null)return dp[i][j];
+
+        int down=triangle.get(i).get(j) + f(i+1,j,n,triangle,dp);
+        int diagonal=triangle.get(i).get(j) + f(i+1,j+1,n,triangle,dp);
+
+        dp[i][j]=Math.min(down,diagonal);
+        return dp[i][j];
+    }
     public int minimumTotal(List<List<Integer>> triangle) {
+        //here multiple ending index so we not start from the last index
+        //it's better to start from 0,0 becuase a single starting point
+        //here only single base caase if reach at the last row return the value
+        //here not out of boundary condition becuase eacch index on top row have diagonal elem in the bottom row
+        //to indentify the dp array size the best way is find the max vallue of row and the max value of column
+
         int n=triangle.size();
-        int[][] dp=new int[n][n];
-        
-        for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle.get(n-1).get(i);
-        }
+        Integer[][] dp=new Integer[n][n];
 
-        //tabulation is always an opposite direction of memoiation
+        return f(0,0,n,triangle,dp);
 
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                int down=triangle.get(i).get(j)+dp[i+1][j];
-                int diagonal=triangle.get(i).get(j) + dp[i+1][j+1];
-
-                dp[i][j]=Math.min(down,diagonal);
-            }
-        }
-
-        return dp[0][0];
     }
 }
